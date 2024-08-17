b3ad286b89f54a2dfb557f419d90a6d464bb444e
asavari381 <70036041+asavari381@users.noreply.github.com>
2024-08-16 20:45:49 -0400

    Add files via upload

---
+++ code-env-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# ----------------------------ASAVARI SHEJWAL
+echo "Setting up terminal session to run code for dissertation"
+source /cs/scratch/agj6/tf2/venv/bin/activate
+cd  ~/Projects/Breast-Cancer-Detection-and-Segmentation/code/src/
